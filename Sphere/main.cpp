49dbda74add3560be85a579db43c420bd9a2e60d
victorsmits <victor.smits@student.vinci.be>
2019-11-06 10:05:41 +0100

    lighted sphere

--- Sphere/main.cpp
+++ Sphere/main.cpp
@@ -39,7 +39,7 @@ const std::vector<const char*> deviceExtensions = {
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
 #else
-const bool enableValidationLayers = false;
+const bool enableValidationLayers = true;
 #endif
 
 VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
